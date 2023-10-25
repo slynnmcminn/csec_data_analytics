3cbb76ce3eaa19641ab1d3aa7284ba99bc68e7df
slynnmcminn <144170644+slynnmcminn@users.noreply.github.com>
2023-10-25 18:04:34 +0000

    slynn1125

---
+++ csec_data_analytics_app/management/commands/Updating_Vulnerabilities.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Updates Vulnerabilities.'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Hello from my custom command!')
