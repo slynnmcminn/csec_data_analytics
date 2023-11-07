3094f9f2c7c6c2663d7b66597e5fddfbacc114cd
slynnmcminn <144170644+slynnmcminn@users.noreply.github.com>
2023-11-07 19:50:59 +0000

    slynn117

--- csec_data_analytics/settings.py
+++ csec_data_analytics/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'mongoengine',
     'rest_framework_mongoengine',
     'drf_spectacular',
+    'other_app',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
