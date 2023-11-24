b56b9b419789787c260b4783fb260649596becd7
slynnmcminn <144170644+slynnmcminn@users.noreply.github.com>
2023-11-24 19:28:37 +0000

    slynn1124

--- csec_data_analytics_app/management/commands/project3_query_nvd.py
+++ csec_data_analytics_app/management/commands/project3_query_nvd.py
@@ -1,12 +1,15 @@
 from django.core.management.base import BaseCommand
-from csec_data_analytics_app.models import CVEVulnerability, Vulnerability
+from csec_data_analytics_app.models import Vulnerability
 from datetime import datetime, timedelta
 
 class Command(BaseCommand):
     help = 'Run queries against the NVD data'
 
+    def query_total_vulnerabilities(self):
+        count = Vulnerability.objects.count()
+        self.stdout.write(f'Total number of vulnerabilities (simple count): {count}')
+
     def handle(self, *args, **options):
-        # Call the methods defined below
         self.query_google_chrome_vulnerabilities()
         self.query_attack_vector_count('NETWORK')
         self.query_attack_vector_count('PHYSICAL')
@@ -14,11 +17,12 @@ class Command(BaseCommand):
         self.query_most_common_weakness()
         self.query_total_vulnerabilities()
 
+
     def query_google_chrome_vulnerabilities(self):
         end_date = datetime.now()
         start_date = end_date - timedelta(days=120)
-        count = CVEVulnerability.objects(
-            vulnerabilities__product='chrome',
+        count = Vulnerability.objects(
+            vulnerable_products__product='Google Chrome',
             publishedDate__gte=start_date,
             publishedDate__lte=end_date
         ).count()
@@ -29,11 +33,11 @@ class Command(BaseCommand):
         self.stdout.write(f"There are {count} vulnerabilities with the attack vector {attack_vector}.")
 
     def get_top_products_with_known_exploit(self, number):
-        # Adjust the logic here based on your data structure and requirements
+        # Implement the logic as needed, based on your application requirements
         pass
 
     def query_most_common_weakness(self):
-        # Implement the logic to find the most common weakness
+        # Implement the logic as needed, based on your application requirements
         pass
 
     def query_total_vulnerabilities(self):
